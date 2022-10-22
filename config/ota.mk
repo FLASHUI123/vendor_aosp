9f7f4f8d0d764290090798294eae4d2676ef5366
ralf979 <ralf979@gmail.com>
2022-10-22 07:43:17 +0000

    ota: update android version

--- config/ota.mk
+++ config/ota.mk
@@ -1,5 +1,5 @@
 ifeq ($(PEX_BUILD_TYPE), OFFICIAL)
-PEX_OTA_VERSION_CODE := twelve
+PEX_OTA_VERSION_CODE := thirteen
 
 PRODUCT_PRODUCT_PROPERTIES += \
     org.pex.ota.version_code=$(PEX_OTA_VERSION_CODE)
